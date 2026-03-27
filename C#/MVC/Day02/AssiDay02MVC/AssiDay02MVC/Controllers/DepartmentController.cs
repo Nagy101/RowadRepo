f6489144bab685664f174c4021300bd405088863
Nagy101 <nagynady1976@gmail.com>
2026-03-27 12:59:21 +0200

    Add Assi Day03

---
+++ C#/MVC/Day02/AssiDay02MVC/AssiDay02MVC/Controllers/DepartmentController.cs
@@ -0,0 +1,78 @@
+﻿using AssiDay02MVC.Data.Contexts;
+using AssiDay02MVC.Models;
+using AssiDay02MVC.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssiDay02MVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public CompanyDbContext Context = new CompanyDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowAll()
+        {
+            return View(Context.Departments.ToList());
+        }
+
+        public IActionResult ShowDetails(int id)
+        {
+            var department = Context.Departments
+                                    .Include(d => d.Teachers)
+                                    .Include(d => d.Courses)
+                                    .Include(d => d.Students)
+                                    .FirstOrDefault(d => d.Id == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                Context.Departments.Add(department);
+                Context.SaveChanges();
+                return RedirectToAction("ShowAll");
+            }
+            return View(department);
+        }
+
+        public IActionResult DeptWithStudents(int id)
+        {
+            var dept = Context.Departments
+                .Include(d => d.Students)
+                .FirstOrDefault(d => d.Id == id);
+
+            if (dept == null)
+                return NotFound();
+
+            var students = dept.Students
+                .Where(s => s.Age > 25)
+                .ToList();
+
+            string state = students.Count > 50 ? "Main" : "Branch";
+
+            var vm = new DepartmentDetailsVM
+            {
+                DepartmentName = dept.Name,
+                Students = students,
+                DepartmentState = state
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ C#/MVC/Day02/AssiDay02MVC/AssiDay02MVC/ViewModel/DepartmentDetailsVM.cs
@@ -0,0 +1,15 @@
+﻿using AssiDay02MVC.Models;
+
+namespace AssiDay02MVC.ViewModel
+{
+    public class DepartmentDetailsVM
+    {
+        public string DepartmentName { get; set; }
+
+        public List<Student>? Students { get; set; }
+
+        public int? SelectedStudentId { get; set; }
+
+        public string DepartmentState { get; set; }
+    }
+}
