237bf2195c65cffa7f5fdb312ec07edc6b631a3c
Nagy101 <nagynady1976@gmail.com>
2026-03-30 07:52:33 +0200

    Add Assi Day04

--- C#/MVC/Day02/AssiDay02MVC/AssiDay02MVC/Controllers/StudentController.cs
+++ C#/MVC/Day02/AssiDay02MVC/AssiDay02MVC/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
 ﻿using AssiDay02MVC.Data.Contexts;
+using AssiDay02MVC.Models;
+using AssiDay02MVC.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,28 +8,124 @@ namespace AssiDay02MVC.Controllers
 {
     public class StudentController : Controller
     {
-        public CompanyDbContext Context = new CompanyDbContext();
-        public IActionResult Index()
-        {
-            return View("ShowAll");
-        }
+        CompanyDbContext _context = new CompanyDbContext();
+
         public IActionResult ShowAll()
         {
-            var students = Context.Students.Include(s=>s.Department).ToList();
+            var students = _context.Students
+                                   .Include(s => s.Department)
+                                   .ToList();
             return View(students);
         }
-        public IActionResult ShowDetails(int Id)
+        public IActionResult ShowDetails(int id)
         {
-            var student = Context.Students.Include(s=>s.Department)
-                                           .Include(s=>s.StuCrs)
-                                           .ThenInclude(sc => sc.Course)
-                                           .FirstOrDefault(s => s.Id == Id);
+            var student = _context.Students
+                                  .Include(s => s.Department)
+                                  .Include(s => s.StuCrs)
+                                  .ThenInclude(sc => sc.Course)
+                                  .FirstOrDefault(s => s.Id == id);
             if (student == null)
-            {
                 return NotFound();
+
+            return View(student);
+        }
+       
+        public IActionResult Add()
+        {
+            var vm = new StudentDeptVM()
+            {
+                Departments = _context.Departments.ToList()
+            };
+            return View(vm);
+        }
+        [HttpPost]
+        public IActionResult Add(StudentDeptVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = new Student()
+                {
+                    Name = vm.Name,
+                    Age = vm.Age,
+                    DepartmentId = vm.DepartmentId
+                };
+
+                _context.Students.Add(student);
+                _context.SaveChanges();
+                return RedirectToAction("ShowAll");
             }
-            return View(student);   
+
+            vm.Departments = _context.Departments.ToList();
+            return View(vm);
+        }
+        
+        public IActionResult Edit(int id)
+        {
+            var student = _context.Students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+                return NotFound();
+
+            var vm = new StudentDeptVM()
+            {
+                Id = student.Id,
+                Name = student.Name,
+                Age = student.Age,
+                DepartmentId = student.DepartmentId,
+                Departments = _context.Departments.ToList()
+            };
+            return View(vm);
+        }
+        [HttpPost]
+        public IActionResult Edit(StudentDeptVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = _context.Students.FirstOrDefault(s => s.Id == vm.Id);
+
+                if (student == null)
+                    return NotFound();
+
+                student.Name = vm.Name;
+                student.Age = vm.Age;
+                student.DepartmentId = vm.DepartmentId;
+
+                _context.SaveChanges();
+                return RedirectToAction("ShowAll");
+            }
+
+            vm.Departments = _context.Departments.ToList();
+            return View(vm);
         }
 
+        public IActionResult Delete(int id)
+        {
+            var student = _context.Students
+                                  .Include(s => s.Department)
+                                  .FirstOrDefault(s => s.Id == id);
+            if (student == null)
+                return NotFound();
+
+            return View(student);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult ConfirmDelete(int id)
+        {
+            var student = _context.Students
+                                  .Include(s => s.StuCrs)  
+                                  .FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+                return NotFound();
+
+            if (student.StuCrs != null && student.StuCrs.Any())
+            {
+                _context.StuCrs.RemoveRange(student.StuCrs);
+            }
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+
+            return RedirectToAction("ShowAll");
+        }
     }
 }
---
+++ C#/MVC/Day02/AssiDay02MVC/AssiDay02MVC/ViewModel/StudentDeptVM.cs
@@ -0,0 +1,16 @@
+﻿using AssiDay02MVC.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace AssiDay02MVC.ViewModel
+{
+    public class StudentDeptVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public int DepartmentId { get; set; }
+        [ValidateNever]
+        public List<Department> Departments { get; set; }
+        public List<StuCrsRes> ? StuCrs { get; set; }
+    }
+}
